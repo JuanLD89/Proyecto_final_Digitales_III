b27b8208541f74550008fd09e94525d98edaf53c
JuanLD89 <62732117+JuanLD89@users.noreply.github.com>
2024-11-09 17:49:28 -0500

    seguridad.c
    Se agregaron nuevas funciones para encender leds de color verde y rojo, además una función para manejar la autenticación y verificar si el usuario y la contraseña se digitaron.
    También se hizo un bucle principal para verificar el correcto funcionamiento de la autenticación.

--- seguridad.c
+++ seguridad.c
@@ -3,45 +3,19 @@
 #include <string.h>
 #include <time.h>
 
-/** Número de filas del teclado matricial. */
 #define ROWS 4
-
-/** Número de columnas del teclado matricial. */
 #define COLS 4
-
-/** Longitud máxima permitida para la clave del usuario (4 caracteres). */
+#define MAX_ID_LENGTH 6
 #define MAX_CLAVE_LENGTH 4
 
-/** Pin asignado al LED rojo, utilizado para indicar acceso denegado. */
 const uint LedRojo = 14;
-
-/** Pin asignado al LED verde, utilizado para indicar acceso concedido. */
 const uint LedVerde = 15;
 
-/** Pin asignado al LED amarillo, utilizado para indicar estado de espera. */
-const uint LedAmarillo = 16;
-
-/** Contador global de intentos fallidos. */
 int numeroIntentos = 0;
 
-/** 
- * Pines de las filas del teclado matricial.
- * Cada valor en el array `rowPins` corresponde a un pin GPIO que controla una fila del teclado.
- */
 const uint8_t rowPins[ROWS] = {9, 8, 7, 6}; 
-
-/** 
- * Pines de las columnas del teclado matricial.
- * Cada valor en el array `colPins` corresponde a un pin GPIO que controla una columna del teclado.
- */
 const uint8_t colPins[COLS] = {2, 5, 4, 3}; 
 
-
-/**
- * Mapa de las teclas del teclado matricial.
- * La matriz `keys` representa el conjunto de teclas de un teclado matricial
- * de 4 filas y 4 columnas. Cada posición corresponde a una tecla específica. 
- */
 char keys[ROWS][COLS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
@@ -50,7 +24,183 @@ char keys[ROWS][COLS] = {
 
 typedef struct
 {
+    char id[MAX_ID_LENGTH + 1];     ///< ID del usuario (hasta 6 dígitos).
     char clave[MAX_CLAVE_LENGTH + 1]; ///< Clave del usuario (hasta 4 dígitos).
-} claves;
+} Usuario;
 
-claves baseDeDatos = {"1111"};
+Usuario baseDeDatos = {"123456", "1111"};
+
+void setup()
+{
+    // Inicializa los pines de las filas como salida
+    for (int i = 0; i < ROWS; i++)
+    {
+        gpio_init(rowPins[i]);
+        gpio_set_dir(rowPins[i], GPIO_OUT);
+        gpio_put(rowPins[i], 1); // Activa la resistencia pull-up
+    }
+
+    // Inicializa los pines de las columnas como entrada
+    for (int i = 0; i < COLS; i++)
+    {
+        gpio_init(colPins[i]);
+        gpio_set_dir(colPins[i], GPIO_IN);
+        gpio_pull_up(colPins[i]); // Activa la resistencia pull-up
+    }
+}
+
+char scanKeypad()
+{
+    for (int row = 0; row < ROWS; row++)
+    {
+        // Activa la fila actual
+        gpio_put(rowPins[row], 0); // Baja a bajo
+        for (int col = 0; col < COLS; col++)
+        {
+            // Lee la columna
+            if (gpio_get(colPins[col]) == 0)
+            {
+                // Espera a que se suelte la tecla
+                sleep_ms(50);
+                if (gpio_get(colPins[col]) == 0)
+                { // Verifica si sigue presionado
+                    // Espera a que se suelte la tecla
+                    while (gpio_get(colPins[col]) == 0)
+                        ;
+                    gpio_put(rowPins[row], 1); // Restaura la fila
+                    return keys[row][col];     // Retorna la tecla presionada
+                }
+            }
+        }
+        gpio_put(rowPins[row], 1); // Restaura la fila
+    }
+    return '\0'; // Sin tecla presionada
+}
+
+int verificarUsuario(const char *id, const char *clave)
+{
+    if (strcmp(baseDeDatos.id, id) == 0 && strcmp(baseDeDatos.clave, clave) == 0)
+    {
+        return 1; // Usuario verificado
+    }
+    return 0; // Verificación fallida
+}
+
+void encenderLedVerde()
+{
+    printf("Acceso concedido.\n");
+    gpio_put(LedVerde, 1);
+    sleep_ms(10000);    //10 segundos prendido (tiempo para abrir la compuerta, si no se vuelve a bloquear)
+    gpio_put(LedVerde, 0);
+    sleep_ms(500);
+}
+
+void encenderLedRojo()
+{
+    printf("Acceso denegado.\n");
+    gpio_put(LedRojo, 1);
+    sleep_ms(2000); //2 segundos prendido (tiempo para indicar clave erronea)
+    gpio_put(LedRojo, 0);
+    sleep_ms(500);
+}
+
+int manejarAutenticacion(char *tempId, char *tempClave)
+{
+
+    printf("Ingrese su ID y clave:\n");
+    int idComplete = 0;
+    int claveComplete = 0;
+    absolute_time_t last_change_time = get_absolute_time();
+    const uint32_t interval = 1000 * 1000; // 1 segundo en microsegundos
+    while (1)
+    {
+
+        char key = scanKeypad();
+        if (key != '\0')
+        {
+            if (!idComplete && strlen(tempId) < MAX_ID_LENGTH)
+            {
+                strncat(tempId, &key, 1); // Añade un solo carácter al ID
+                if (strlen(tempId) == MAX_ID_LENGTH)
+                {
+                    idComplete = 1; // ID completo
+                }
+            }
+            else if (idComplete && !claveComplete && strlen(tempClave) < MAX_CLAVE_LENGTH)
+            {
+                strncat(tempClave, &key, 1); // Añade un solo carácter a la clave
+                if (strlen(tempClave) == MAX_CLAVE_LENGTH)
+                {
+                    claveComplete = 1; // Clave completa
+                }
+            }
+        }
+        if (idComplete)
+        {
+            if (absolute_time_diff_us(last_change_time, get_absolute_time()) >= interval)
+            {
+                // Actualiza el tiempo del último cambio
+                last_change_time = get_absolute_time();
+            }
+            
+        }
+
+        if (idComplete && claveComplete)
+        {
+            break; // Salir si se completaron ID y clave
+        }
+    }
+
+    // Mostrar ID y clave ingresados
+    printf("ID ingresado: %s\n", tempId);
+    printf("Clave ingresada: %s\n", tempClave);
+
+    if (verificarUsuario(tempId, tempClave))
+    {
+        encenderLedVerde();
+        return 1; // Autenticación exitosa
+    }
+    else
+    {
+        encenderLedRojo();
+        return 0; // Falló la autenticación
+    }
+
+}
+
+int main()
+{
+    stdio_init_all();
+    setup();
+
+    // Configura el pin como salida
+    gpio_init(LedRojo);
+    gpio_set_dir(LedRojo, GPIO_OUT);
+    gpio_init(LedVerde);
+    gpio_set_dir(LedVerde, GPIO_OUT);
+
+    while (true)
+    {
+        char tempId[MAX_ID_LENGTH + 1] = {0};
+        char tempClave[MAX_CLAVE_LENGTH + 1] = {0};
+        char keyinicial = scanKeypad();
+
+        if (keyinicial != '\0')
+        {
+            printf("Tecla presionada: %c\n", keyinicial);
+            if (keyinicial == '*')
+            {
+                if (manejarAutenticacion(tempId, tempClave))
+                {
+                    printf("Usuario autenticado.\n");
+                }
+                else
+                {
+                    printf("Fallo en la autenticación.\n");
+                }
+            }
+        }
+    }
+
+    return 0;
+}
