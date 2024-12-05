fb7c1c0acf3cff2f0e625e00a57405404864cdc9
DuvanRamirez <125146393+DuvanRamirez@users.noreply.github.com>
2024-12-05 16:35:38 -0500

    Se agrego nuevas funciones

--- Dispensador_de_comida/main.c
+++ Dispensador_de_comida/main.c
@@ -1,11 +1,15 @@
 #include "src/sensor_proximidad.h"
 #include "src/intervalos_tiempo.h"
 #include "src/configuracion_tiempo_teclado.h"
+#include "src/mostrar_en_displays.h"
+#include "src/configuracion_porcion_teclado.h"
+#include "src/funcionamiento_motor.h"
 
 int main()
 {
     stdio_init_all(); // Inicializar la consola
     init_pins();      // Configurar los pines
+    init_pins2();
     setup();
 
     // Inicializar el RTC
@@ -20,10 +24,14 @@ int main()
 
     rtc_init();
     rtc_set_datetime(&t);
+    uint slice_num, channel;
+    init_servo(&slice_num, &channel);
 
+    bool motor = false;
     bool medir = false;
+    bool inicio = true;
     alarm_interval_hours = 0;    // Configurar horas
-    alarm_interval_minutes = 0;  // Configurar minutos
+    alarm_interval_minutes = 1;  // Configurar minutos
     alarm_interval_seconds = 0; // Configurar segundos
 
     printf("Inicio del programa\n");
@@ -64,16 +72,20 @@ int main()
             // La alarma se activo, realizar accion
             alarm_triggered = false;
             // ejecucion
-            for (int i = 0; i < 5; i++)
+            motor = true;
+
+            if (motor)
             {
-                printf("Ejecucion...\n");
-                sleep_ms(500);
+                set_servo_speed(slice_num, channel, 1.0f);   // Máxima velocidad sentido horario
+                sleep_ms(3000);                              // Mantener por 3 segundos
+                set_servo_speed(slice_num, channel, 0.01f); // Detener el servo
+                motor = false;                               // Resetear la variable
             }
         }
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        while(key_pressed)
+        while (key_pressed)
         {
             // Leer la tecla presionada
             char key = scanKeypad();
@@ -81,7 +93,7 @@ int main()
             {
                 printf("Tecla presionada: %c\n", key);
 
-                //configurar hora
+                // configurar hora
                 if (key == 'A') // Inicia configuración
                 {
                     char horas[3] = {0};   // Buffer para horas
@@ -90,17 +102,33 @@ int main()
                     int horas_int = atoi(horas);
                     int minutos_int = atoi(minutos);
                     printf("Hora configurada: %d:%d\n", horas_int, minutos_int);
-                    alarm_interval_hours = horas_int;    // Configurar horas
-                    alarm_interval_minutes = minutos_int;  // Configurar minutos
-                    alarm_interval_seconds = 0; // Configurar segundos
+                    alarm_interval_hours = horas_int;     // Configurar horas
+                    alarm_interval_minutes = minutos_int; // Configurar minutos
+                    alarm_interval_seconds = 0;           // Configurar segundos
                     // Restablecer la bandera
                     key_pressed = false;
                 }
-                 if (key == 'D')
-                 {
+                //configuracion porcion
+                else if (key == 'B') // Inicia configuración
+                {
+                    char porcion[5] = {0}; // Buffer para la porción
+                    configurar_porcion(porcion);
+                    printf("Porción válida: %d gramos\n", gramos);
+                    key_pressed = false;
+                }
+
+                else if (key == 'D')
+                {
                     // Restablecer la bandera
                     key_pressed = false;
-                 }
+                }
+            }
+            if (inicio == true)
+            {
+                // Restablecer la bandera
+                key_pressed = false;
+                inicio = false;
+                break;
             }
         }
 
